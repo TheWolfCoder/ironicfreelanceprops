3518ab397d52f1507f2841d52b4ef36f51c5a4d9
Stephan Brown <stephan.r.brown@gmail.com>
2017-04-24 01:32:31 +0000

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@
     customer: "Customer #{proposal}",
     portfolio_url: "shielded-eyrie-58623.herokuapp.com/portfolios" ,
     tools: "Ruby on Rails, Angular 2, Postgres",
-    estimated_hours: 80 + (proposal),
+    estimated_hours: (80 + proposal),
     hourly_rate: 120,
     weeks_to_complete: 12,
     client_email: "john@example.com",
